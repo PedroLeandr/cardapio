6b9fb210fd7818347d02617e4dd554bc75bfe09e
PedroLeandr <pedroleandrovieiranascimento@gmail.com>
2026-05-11 11:25:53 +0100

    best design /[slug]

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,23 @@
+import { Cormorant_Garamond, Outfit } from "next/font/google"
+
+const cormorant = Cormorant_Garamond({
+  subsets: ["latin"],
+  weight: ["300", "400", "600", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-cormorant",
+  display: "swap",
+})
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  variable: "--font-outfit",
+  display: "swap",
+})
+
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={`${cormorant.variable} ${outfit.variable}`}>
+      {children}
+    </div>
+  )
+}
